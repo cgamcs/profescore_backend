fffb44b29c1e8662d1dabcd91dc56bedc76d4aaa
César García <cesar.garciamcs@outlook.com>
2025-03-27 18:15:45 -0600

    verificando api de fingerprint server2

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // Endpoint para manejar las solicitudes de FingerprintJS
-app.post('/api', (req, res) => {
+app.post('/api/fingerprint', (req, res) => {
   console.log('Datos del visitante:', req.body);
   res.status(200).json({ message: 'Datos del visitante recibidos' });
 });
