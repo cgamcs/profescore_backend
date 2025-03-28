37e55ae2dfdd18ca68937cf670dc52973077d010
César García <cesar.garciamcs@outlook.com>
2025-03-27 18:07:19 -0600

    verificando api de fingerprint server

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,25 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from 'dotenv';
+dotenv.config();
 
-import express from 'express'
-import cors from 'cors'
-import corsOptions from './config/cors'
-import { connectDB } from './config/db'
-import facultyRoutes from './routes/facultyRoutes'
-import adminRoutes from './routes/adminRoutes'
+import express from 'express';
+import cors from 'cors';
+import corsOptions from './config/cors';
+import { connectDB } from './config/db';
+import facultyRoutes from './routes/facultyRoutes';
+import adminRoutes from './routes/adminRoutes';
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
+app.use(express.json());
 
-app.use(express.json())
+// Endpoint para manejar las solicitudes de FingerprintJS
+app.post('/api', (req, res) => {
+  console.log('Datos del visitante:', req.body);
+  res.status(200).json({ message: 'Datos del visitante recibidos' });
+});
 
 // Rutas públicas
 app.use('/api/faculties', facultyRoutes);
@@ -22,4 +27,4 @@ app.use('/api/faculties', facultyRoutes);
 // Rutas administrativas (login y operaciones protegidas)
 app.use('/api/admin', adminRoutes);
 
-export default app
+export default app;
